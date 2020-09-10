39ecb27c89e1a131723f6142eb1ab82dea65df24
nadya <nadya@yandex.ru>
2020-09-11 00:20:23 +0300

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -16,29 +16,31 @@ class MyApp extends StatelessWidget{
       appBar: AppBar(
           title: Text("Категории")
       ),
-      body: Column(
+      body:
+        Column(
           children: [
             SizedBox(
               width: double.infinity,
-              child: RaisedButton( color: Color(0xFF64B5F6),textColor: Colors.white,
-                child: Text("Кубизм",style:TextStyle(fontSize: 20)),
+              child:
+              RaisedButton( color: Color(0xFF64B5F6),textColor: Colors.white,
+                child: Text("Литература",style:TextStyle(fontSize: 20)),
                 onPressed:() {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => Begin("Кубизм")),
+                    MaterialPageRoute(builder: (context) => Begin("Литература")),
                   );
                 }
-                )
+              )
             ),
 
             SizedBox(
                 width: double.infinity,
                 child: RaisedButton( color: Color(0xFF64B5F6),textColor: Colors.white,
-                    child: Text("Импрессионизм",style:TextStyle(fontSize: 20)),
+                    child: Text("Физика",style:TextStyle(fontSize: 20)),
                     onPressed:() {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Begin("Импрессионизм")),
+                        MaterialPageRoute(builder: (context) => Begin("Физика")),
                       );
                     }
                 )
@@ -47,16 +49,15 @@ class MyApp extends StatelessWidget{
             SizedBox(
                 width: double.infinity,
                 child: RaisedButton( color: Color(0xFF64B5F6),textColor: Colors.white,
-                    child: Text("Абстракционизм",style:TextStyle(fontSize: 20)),
+                    child: Text("Философия",style:TextStyle(fontSize: 20)),
                     onPressed:() {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Begin("Абстракционизм")),
+                        MaterialPageRoute(builder: (context) => Begin("Философия")),
                       );
                     }
                 )
             ),
-
           ]
       )
     );
@@ -71,18 +72,19 @@ class Begin extends StatelessWidget{
       appBar: AppBar(automaticallyImplyLeading: false,
         title: Text(data),
     ),
-    body: Column(mainAxisAlignment: MainAxisAlignment.center, mainAxisSize: MainAxisSize.max,
+    body:
+      Column(mainAxisAlignment: MainAxisAlignment.center, mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(height: 200.0,
-        child:Text("Давайте проверим Ваши знания в данной категории", style: TextStyle(fontSize: 23),textAlign: TextAlign.center,)),
+        children: [
+          Container(height: 200.0,
+          child:Text("Давайте проверим Ваши знания в данной категории", style: TextStyle(fontSize: 23),textAlign: TextAlign.center,)),
       RaisedButton(color: Color(0xFF64B5F6),textColor: Colors.white,
         onPressed: () {
           Navigator.pushAndRemoveUntil(
             context,
             MaterialPageRoute(builder: (context) => quiz(data)),
                 (Route<dynamic> route) => false,
-          );// Navigator.push(context);
+          );
         },
         child: Text('Начать'),
       ),
--- lib/result.dart
+++ lib/result.dart
@@ -5,18 +5,23 @@ class Result extends StatelessWidget{
   final int result_score;
   Result(this.result_score);
   String get result_phrase{
-    if (result_score>1)return 'good job';
-    else return 'try again';
+    if (result_score>4)return '$result_score/5\n Отличный результат';
+    else if(result_score>2)return '$result_score/5\nНеплохо';
+    else return '$result_score/5\nПопробуйте ещё раз';
 }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:[Text(result_phrase),
-      RaisedButton (child: Text("to categories"),
-        onPressed: (){Navigator.pushNamedAndRemoveUntil(context, '/', (Route<dynamic> route)=>false);}//(){ Navigator.pushNamed(context, '/');},
-      )
-      ] ,
+    return
+      Column(mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+          Container(height: 200.0,width: double.infinity,
+            child:
+              Text(result_phrase, style: TextStyle(fontSize: 28),textAlign: TextAlign.center,)
+          ),
+          RaisedButton (child: Text("К категориям"),
+            onPressed: (){Navigator.pushNamedAndRemoveUntil(context, '/', (Route<dynamic> route)=>false);}
+          )
+        ] ,
     );
-
   }
 }
